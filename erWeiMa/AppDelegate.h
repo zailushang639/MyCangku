40d9e18023b146497c1bcb97d3a87c8be4cd49e6
票金所 <Pjs@ownwifi.local>
2016-06-17 10:39:51 +0800

    Initial Commit

---
+++ erWeiMa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  erWeiMa
+//
+//  Created by 票金所 on 16/6/17.
+//  Copyright © 2016年 杨晨晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ erWeiMa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  erWeiMa
+//
+//  Created by 票金所 on 16/6/17.
+//  Copyright © 2016年 杨晨晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
